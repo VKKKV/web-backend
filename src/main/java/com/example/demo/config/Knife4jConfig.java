0e7929536fb18133403971227c5a8fc64178a33d
vkkkv <1165652563@qq.com>
2025-03-12 00:56:17 +0800

    refactor: change Knife4jConfig api

--- src/main/java/com/example/demo/config/Knife4jConfig.java
+++ src/main/java/com/example/demo/config/Knife4jConfig.java
@@ -1,13 +1,7 @@
 package com.example.demo.config;
 
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,36 +11,44 @@ public class Knife4jConfig {
 
     @Bean
     public OpenAPI customOpenApi() {
-        return new OpenAPI()
-                .info(new Info()
+
+        return new OpenAPI().info(
+                new Info()
                         .title("股票交易平台 API 文档")
-                        .description("基于 Knife4j + Spring Boot 的接口文档")
-                        .version("v1.0.0")
-                        .contact(new Contact().name("zy")))
-                .externalDocs(new ExternalDocumentation()
-                        .description("GitHub 仓库")
-                        .url("https://github.com/VKKKV/web-backend"));
+                        .version("v1.0")
+                        .description("基于 Knife4j + Spring Boot 的接口文档"));
     }
 
     @Bean
     public GroupedOpenApi userApi() {
-        return GroupedOpenApi.builder().group("User").
-                pathsToMatch("/api/v1/users/**").
-                build();
+        return GroupedOpenApi.builder()
+                .group("用户管理")
+                .pathsToMatch("/api/v1/users/**")
+                .build();
     }
 
     @Bean
     public GroupedOpenApi tradeApi() {
-        return GroupedOpenApi.builder().group("Trade").
-                pathsToMatch("/api/v1/trades/**").
-                build();
+        return GroupedOpenApi.builder()
+                .group("交易管理")
+                .pathsToMatch("/api/v1/trade/**")
+                .build();
     }
 
     @Bean
-    public GroupedOpenApi stockApi() {
-        return GroupedOpenApi.builder().group("Stock").
-                pathsToMatch("/api/v1/strategy/**").
-                build();
+    public GroupedOpenApi marketApi() {
+        return GroupedOpenApi.builder()
+                .group("行情数据")
+                .pathsToMatch("/api/v1/market/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi strategyApi() {
+        return GroupedOpenApi.builder()
+                .group("策略管理")
+                .pathsToMatch("/api/v1/strategy/**")
+                .build();
     }
 }
 
