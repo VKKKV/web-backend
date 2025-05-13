6ded9ca38b1bcb630e68965cffb0de6e72b30cec
vkkkv <1165652563@qq.com>
2025-05-14 03:41:56 +0800

    feat: add TransactionsServiceImpl balance

--- src/main/java/com/example/demo/service/impl/TransactionsServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/TransactionsServiceImpl.java
@@ -2,15 +2,12 @@ package com.example.demo.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
-import com.baomidou.mybatisplus.core.toolkit.StringUtils;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import com.example.demo.entity.Stocks;
 import com.example.demo.entity.Transactions;
-import com.example.demo.service.StocksService;
 import com.example.demo.service.TransactionsService;
+import com.example.demo.service.UsersService;
 import com.example.demo.mapper.TransactionsMapper;
 import com.example.demo.vo.OrderRequestVO;
-import io.swagger.v3.oas.annotations.media.Schema;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -27,6 +24,9 @@ public class TransactionsServiceImpl extends ServiceImpl<TransactionsMapper, Tra
     @Autowired
     private TransactionsMapper transactionsMapper;
 
+    @Autowired
+    private UsersService usersService;
+
     @Override
     public IPage<Transactions> getHistoryPage(IPage<Transactions> page, Integer userId) {
         LambdaQueryWrapper<Transactions> wrapper = new LambdaQueryWrapper<>();
@@ -39,14 +39,30 @@ public class TransactionsServiceImpl extends ServiceImpl<TransactionsMapper, Tra
 
     @Override
     public void createOrder(OrderRequestVO orderRequest) {
+        Integer userId = orderRequest.getUserId();
+        String type = orderRequest.getType();
+        BigDecimal price = BigDecimal.valueOf(orderRequest.getPrice());
+        BigDecimal quantity = new BigDecimal(orderRequest.getQuantity().toString());
+        BigDecimal totalAmount = price.multiply(quantity);
+
+        if ("BUY".equalsIgnoreCase(type)) {
+            usersService.updateUserBalance(userId, totalAmount.negate());
+
+        } else if ("SELL".equalsIgnoreCase(type)) {
+            usersService.updateUserBalance(userId, totalAmount);
+
+        } else {
+            throw new com.example.demo.common.exception.TradeException("无效的交易类型", com.example.demo.common.result.ResultCodeEnum.PARAM_ERROR.getCode());
+        }
+
         Transactions transactions = new Transactions();
         transactions.setStockId(0);
-        transactions.setUserId(orderRequest.getUserId());
+        transactions.setUserId(userId);
         transactions.setStockCode(orderRequest.getStockCode());
-        transactions.setActionType(orderRequest.getType());
+        transactions.setActionType(type.toUpperCase());
         transactions.setQuantity(orderRequest.getQuantity());
-        transactions.setPrice(BigDecimal.valueOf(orderRequest.getPrice()));
-        transactions.setStatus("PENDING");
+        transactions.setPrice(price);
+        transactions.setStatus("EXECUTED");
         transactions.setOrderType(orderRequest.getOrderType());
 
         transactionsMapper.insert(transactions);
